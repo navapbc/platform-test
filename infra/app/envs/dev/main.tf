3c5cc6b156d5afbb5b90612d734f476d9a1b70ee
Loren Yu <loren@navapbc.com>
2022-11-03 13:39:10 -0700

    Initialize tf backends for app envs

--- infra/app/envs/dev/main.tf
+++ infra/app/envs/dev/main.tf
@@ -28,10 +28,10 @@ terraform {
   # Terraform does not allow interpolation here, values must be hardcoded.
 
   backend "s3" {
-    bucket         = "<TF_STATE_BUCKET_NAME>"
-    key            = "infra/<APP_NAME>/environments/dev.tfstate"
-    dynamodb_table = "<TF_LOCKS_TABLE_NAME>"
-    region         = "<REGION>"
+    bucket         = "platform-test-430004246987-us-east-1-tf-state"
+    key            = "infra/app/environments/dev.tfstate"
+    dynamodb_table = "platform-test-tf-state-locks"
+    region         = "us-east-1"
     encrypt        = "true"
   }
 }

88b8a3356a179814764b32eef4fb9b011db86da6
Michael Sanderson <michaelsanderson@navapbc.com>
2023-04-11 12:20:04 -0400

    Implement return early pattern

--- bin/publish-release.sh
+++ bin/publish-release.sh
@@ -25,14 +25,14 @@ echo "Authenticating Docker with ECR"
 aws ecr get-login-password --region $REGION \
   | docker login --username AWS --password-stdin $IMAGE_REGISTRY
 echo
-echo "Verify if tag has already been published"
+echo "Check if tag has already been published..."
 aws ecr describe-images --repository-name $IMAGE_NAME --image-ids imageTag=$IMAGE_TAG --region $REGION > /dev/null 2>&1
 IMAGE_TAG_EXISTS=$(echo $?)
 if [ $IMAGE_TAG_EXISTS -eq 0 ];then
   echo "Image with tag $IMAGE_TAG already published"
-else
-  echo "New tag. Publishing image"
-  docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_REPOSITORY_URL:$IMAGE_TAG
-  docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
+  exit 0
 fi
+echo "New tag. Publishing image"
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_REPOSITORY_URL:$IMAGE_TAG
+docker push $IMAGE_REPOSITORY_URL:$IMAGE_TAG
 

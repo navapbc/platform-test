805f41f0a70cd0477e324138a7005b4579f8de88
Loren Yu <loren@navapbc.com>
2023-04-10 17:51:58 -0700

    Import psycopg

--- infra/modules/database/role_checker/role_checker.py
+++ infra/modules/database/role_checker/role_checker.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import boto3
+import psycopg
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,4 +9,5 @@ def lambda_handler(event, context):
     print(os.environ.get("DB_HOST"))
     print(os.environ.get("DB_PORT"))
     print(os.environ.get("DB_USER"))
+    print(psycopg)
     return "Hello, world"

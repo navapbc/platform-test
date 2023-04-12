f3b123b2ab75fcb91d14b09e6f36ce5a9c2910e2
Loren Yu <loren@navapbc.com>
2023-04-12 10:58:37 -0700

    Add dbhealth to test server

--- app/start-server.sh
+++ app/start-server.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+export PGPASSWORD="$(aws rds generate-db-auth-token --hostname $DB_HOST --port $DB_PORT --region $AWS_REGION --username $DB_USER )"
+
+echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASSWORD" > ~/.pgpass
+
+psql "sslmode=verify-full sslrootcert=full_path_to_ssl_certificate" -c "SELECT 'Connected to db';" > /www/dbhealth
+
 echo "httpd started on port $PORT" && trap "exit 0;" TERM INT; httpd -v -p $PORT -h /www -f & wait

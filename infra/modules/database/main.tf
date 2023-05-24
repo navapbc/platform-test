187b49ffcb58a328a14f62f996d63169a60b7b81
Loren Yu <loren@navapbc.com>
2023-05-24 15:33:51 -0700

    Put role manager security group back

--- infra/modules/database/main.tf
+++ infra/modules/database/main.tf
@@ -82,6 +82,32 @@ resource "aws_security_group" "db" {
   vpc_id      = var.vpc_id
 }
 
+resource "aws_security_group" "role_manager" {
+  name_prefix = "${var.name}-role-manager"
+  description = "Database role manager security group"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_vpc_security_group_egress_rule" "role_manager_egress_to_db" {
+  security_group_id = aws_security_group.role_manager.id
+  description       = "Allow role manager to access database"
+
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.db.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_ingress_from_role_manager" {
+  security_group_id = aws_security_group.db.id
+  description       = "Allow inbound requests to database from role manager"
+
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.role_manager.id
+}
+
 #----------------#
 # Authentication #
 #----------------#
@@ -244,7 +270,7 @@ resource "aws_lambda_function" "role_manager" {
 
   vpc_config {
     subnet_ids         = var.private_subnet_ids
-    security_group_ids = [aws_security_group.db.id]
+    security_group_ids = [aws_security_group.role_manager.id]
   }
 
   environment {

c97fa80f56d63199125e641863168264cc01c707
Loren Yu <loren@navapbc.com>
2023-06-08 08:39:04 -0700

    Remove unneeded security group

--- infra/modules/service/outputs.tf
+++ infra/modules/service/outputs.tf
@@ -2,8 +2,3 @@ output "public_endpoint" {
   description = "The public endpoint for the service."
   value       = "http://${aws_lb.alb.dns_name}"
 }
-
-output "app_security_group_id" {
-  description = "The ID of the security group for the application layer."
-  value       = aws_security_group.app.id
-}

076bbf9f5440ccf95e9aef072c5a67ed569a8987
Loren Yu <loren@navapbc.com>
2023-06-14 09:26:42 -0700

    Refresh app-config

--- bin/run-database-migrations.sh
+++ bin/run-database-migrations.sh
@@ -29,6 +29,7 @@ echo
 
 # Step 0. Check if app has a database
 
+terraform -chdir=infra/$APP_NAME/app-config refresh > /dev/null
 HAS_DATABASE=$(terraform -chdir=infra/$APP_NAME/app-config output -raw has_database)
 if [ $HAS_DATABASE = "false" ]; then
   echo "Application does not have a database, no migrations to run"

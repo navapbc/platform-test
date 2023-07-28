c0d8b5b79d2b48404c5af85b9a211e9bfaf49455
FJOR <charlesfraley@navapbc.com>
2023-07-28 11:06:11 -0500

    Moved s3 bucket to module

--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -108,7 +108,6 @@ resource "aws_lb_listener_rule" "app_http_forward" {
   }
 }
 
-
 resource "aws_lb_target_group" "app_tg" {
   # you must use a prefix, to facilitate successful tg changes
   name_prefix          = "app-"
@@ -136,64 +135,14 @@ resource "aws_lb_target_group" "app_tg" {
 #--------------------------
 ## Load balancer log infra
 #--------------------------
-resource "aws_s3_bucket" "load_balancer_logs" {
-  bucket_prefix = "${var.service_name}-access-logs"
-  force_destroy = false
-  # checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
-  # checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
-}
-
-resource "s3_bucket_public_access_block" "load_balancer_logs" {
-  bucket = aws_s3_bucket.load_balancer_logs.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_replication_configuration" "load_balancer_logs" {
+module "alb_log_s3" {
+  source = "../compliant-s3"
   
-}
-
-# resource "aws_s3_bucket_lifecycle_configuration" "load_balancer_logs" {
-#   count = var.log_file_transition != [] || var.log_file_deletion !=0 ? 1 : 0
-#   bucket =   aws_s3_bucket.load_balancer_logs.id
-#   rule {
-#     id = "Logfile Lifecycle"
-#     filter {
-#       prefix = "${var.service_name}-lb"
-#       dynamic "transition" {
-#         for_each = var.log_file_transition
-#         content {
-
-#         }
-#       }
-#     }
-#   }
-# }
-
-resource "aws_s3_bucket_versioning" "load_balancer_logs" {
-  bucket = aws_s3_bucket.load_balancer_logs.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "load_balancer" {
-  bucket = aws_s3_bucket.load_balancer_logs.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-    }
-    bucket_key_enabled = true
-  }
-}
-
-resource "aws_s3_bucket_policy" "load_balancer_logs_put_access" {
-  bucket = aws_s3_bucket.load_balancer_logs.id
-  policy = data.aws_iam_policy_document.load_balancer_logs_put_access.json
+  bucket_policy_document = data.aws_iam_policy_document.load_balancer_logs_put_access.json
+  service_name = var.service_name
+  ia_storage_after = var.ia_storage_after
+  glacier_storage_after = var.glacier_storage_after
+  delete_objects_after = var.delete_objects_after
 }
 
 data "aws_iam_policy_document" "load_balancer_logs_put_access" {

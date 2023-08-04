27a638db752159b0b61b7401c5b58981df3ef983
FJOR <charlesfraley@navapbc.com>
2023-08-04 11:20:38 -0500

    Format

--- infra/modules/compliant-s3/main.tf
+++ infra/modules/compliant-s3/main.tf
@@ -99,7 +99,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "lc" {
     }
   }
   rule {
-    id = "AbortIncompleteUpload"
+    id     = "AbortIncompleteUpload"
     status = "Enabled"
     abort_incomplete_multipart_upload {
       days_after_initiation = 7

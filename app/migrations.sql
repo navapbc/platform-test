58a2a67106fd6247a2170637600966ee89b1783a
Loren Yu <loren@navapbc.com>
2023-08-10 09:12:32 -0700

    Alter default privleges in migrations file

--- app/migrations.sql
+++ app/migrations.sql
@@ -1,10 +1,12 @@
+-- Hardcoded the app username since couldn't figure out
+-- a simple way to pull it from environment variables
+ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO app;
+
 DROP TABLE IF EXISTS migrations;
 
 CREATE TABLE IF NOT EXISTS migrations (
     last_migration_date TIMESTAMP
 );
-
-TRUNCATE TABLE migrations;
 
 INSERT INTO migrations (last_migration_date)
 VALUES (NOW());

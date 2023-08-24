4a4a4601d2c0505237048192e30ae5213d1c4bcf
FJOR <charlesfraley@navapbc.com>
2023-08-23 19:07:49 -0500

    Add db app access policy data object To fix missing reference error

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -86,7 +86,7 @@ module "service" {
 
   db_vars = module.app_config.has_database ? {
     security_group_ids = data.aws_rds_cluster.db_cluster[0].vpc_security_group_ids
-    access_policy_arn  = aws_iam_policy.db_app_access_policy[0].arn
+    app_access_policy_arn  = aws_iam_policy.db_app_access_policy[0].arn
     connection_info = {
       host        = data.aws_rds_cluster.db_cluster[0].endpoint
       port        = data.aws_rds_cluster.db_cluster[0].port
@@ -95,6 +95,11 @@ module "service" {
       schema_name = local.database_config.schema_name
     }
   } : null
+}
+
+data "aws_iam_policy" "db_app_access_policy" {
+  count              = module.app_config.has_database ? 1 : 0
+  name = module.app_config.app_access_policy_name
 }
 
 module "monitoring" {

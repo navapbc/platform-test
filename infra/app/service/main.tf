bcf5f68d0cc30070029edc24f437dedcb448f992
FJOR <charlesfraley@navapbc.com>
2023-08-24 22:00:39 -0500

    Change data object to same reference as database config object

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -86,8 +86,8 @@ module "service" {
 
   db_vars = module.app_config.has_database ? {
     security_group_ids         = data.aws_rds_cluster.db_cluster[0].vpc_security_group_ids
-    app_access_policy_arn      = aws_iam_policy.db_app_access_policy[0].arn
-    migrator_access_policy_arn = aws_iam_policy.db_migrator_access_policy[0].arn
+    app_access_policy_arn      = data.aws_iam_policy.db_app_access_policy[0].arn
+    migrator_access_policy_arn = data.aws_iam_policy.db_migrator_access_policy[0].arn
     connection_info = {
       host        = data.aws_rds_cluster.db_cluster[0].endpoint
       port        = data.aws_rds_cluster.db_cluster[0].port
@@ -100,12 +100,12 @@ module "service" {
 
 data "aws_iam_policy" "db_app_access_policy" {
   count = module.app_config.has_database ? 1 : 0
-  name  = module.app_config.app_access_policy_name
+  name  =  "${local.prefix}${local.database_config.app_access_policy_name}"
 }
 
 data "aws_iam_policy" "db_migrator_access_policy" {
   count = module.app_config.has_database ? 1 : 0
-  name  = module.app_config.app_access_policy_name
+  name  = "${local.prefix}${local.database_config.migrator_access_policy_name}"
 }
 
 module "monitoring" {

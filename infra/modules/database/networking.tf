ac261a06a8f64d17f088c5abef975980ffb184dc
Loren Yu <loren@navapbc.com>
2023-08-25 15:15:23 -0700

    Update access to vpc endpoint security group

--- infra/modules/database/networking.tf
+++ infra/modules/database/networking.tf
@@ -40,11 +40,11 @@ resource "aws_vpc_security_group_egress_rule" "role_manager_egress_to_vpc_endpoi
   from_port                    = 443
   to_port                      = 443
   ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.vpc_endpoints.id
+  referenced_security_group_id = var.aws_services_security_group_id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpc_endpoints_ingress_from_role_manager" {
-  security_group_id = aws_security_group.vpc_endpoints.id
+  security_group_id = var.aws_services_security_group_id
   description       = "Allow inbound requests to VPC endpoints from role manager"
 
   from_port                    = 443

ad69204d888c928d1e4b56ddb71021811807c009
FJOR <charlesfraley@navapbc.com>
2023-08-28 14:11:04 -0500

    Add documentation from infra-test

--- infra/app/app-config/main.tf
+++ infra/app/app-config/main.tf
@@ -15,7 +15,10 @@ locals {
   #   <ACCOUNT_NAME>.<ACCOUNT_ID>.s3.tfbackend
   #
   # Projects/applications that use the same AWS account for all environments
-  # will refer to the same account for all environments:
+  # will refer to the same account for all environments. For example, if the
+  # project has a single account named "myaccount", then infra/accounts will
+  # have one tfbackend file myaccount.XXXXX.s3.tfbackend, and the 
+  # account_names_by_environment map will look like:
   #
   #   account_names_by_environment = {
   #     shared  = "shared"

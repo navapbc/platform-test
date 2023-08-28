78299bba6a6aa5a1602e27bd61e7c7c6e60e2888
FJOR <charlesfraley@navapbc.com>
2023-08-28 14:16:51 -0500

    Make formatting match template-infra

--- infra/modules/service/access-control.tf
+++ infra/modules/service/access-control.tf
@@ -22,7 +22,6 @@ resource "aws_iam_role" "migrator_service" {
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
 }
 
-
 data "aws_iam_policy_document" "ecs_tasks_assume_role_policy" {
   statement {
     sid = "ECSTasksAssumeRole"

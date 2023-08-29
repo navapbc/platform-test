3969211dd7d0149cd4e97496cb368362657f0512
Loren Yu <loren@navapbc.com>
2023-08-29 13:30:18 -0700

    Add logs

--- infra/test/database_test.go
+++ infra/test/database_test.go
@@ -15,11 +15,15 @@ func TestDatabase(t *testing.T) {
 		VarFiles:     []string{"dev.tfvars"},
 	})
 
+	fmt.Println("::group::Initializing database module")
 	TerraformInit(t, terraformOptions, "dev.s3.tfbackend")
+	fmt.Println("::endgroup::")
 
 	workspaceName := RandomWorkspaceName()
 	defer terraform.WorkspaceDelete(t, terraformOptions, workspaceName)
+	fmt.Println("::group::Creating new workspace")
 	terraform.WorkspaceSelectOrNew(t, terraformOptions, workspaceName)
+	fmt.Println("::endgroup::")
 
 	defer DestroyDatabase(t, terraformOptions)
 	terraform.Apply(t, terraformOptions)

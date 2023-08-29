23235f4fd10ca64bf79915801ed3a6fd74938289
FJOR <charlesfraley@navapbc.com>
2023-08-29 10:46:52 -0500

    Rename migrator_service to migrator_task

--- infra/modules/service/database-access.tf
+++ infra/modules/service/database-access.tf
@@ -14,7 +14,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_ingress_from_service" {
   referenced_security_group_id = aws_security_group.app.id
 }
 
-
 resource "aws_iam_role_policy_attachment" "app_db_access" {
   count = var.db_vars != null ? 1 : 0
 
@@ -32,6 +31,6 @@ resource "aws_iam_role_policy_attachment" "app_service_db_access" {
 resource "aws_iam_role_policy_attachment" "migrator_db_access" {
   count = var.db_vars != null ? 1 : 0
 
-  role       = aws_iam_role.migrator_service.name
+  role       = aws_iam_role.migrator_task.name
   policy_arn = var.db_vars.migrator_access_policy_arn
 }

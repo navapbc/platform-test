c25a6e1ac945329e5660ac5ad89e18a61c824644
Loren Yu <loren@navapbc.com>
2023-09-01 16:55:12 -0700

    Return json

--- bin/get-account-id-by-name.sh
+++ bin/get-account-id-by-name.sh
@@ -5,9 +5,17 @@
 set -euo pipefail
 ACCOUNT_NAME=$1
 
+# We use script dir to make this script agnostic to where it's called from.
+# This is needed since this script its called from infra/<app>/build-repository
+# in an external data source
 SCRIPT_DIR=$(dirname $0)
-BACKEND_CONFIG_FILE_PATH=$(ls -1 "$SCRIPT_DIR"/../infra/accounts/"$ACCOUNT_NAME".*.s3.tfbackend)
-BACKEND_CONFIG_FILE=$(basename "$BACKEND_CONFIG_FILE_PATH")
-BACKEND_CONFIG_NAME="${BACKEND_CONFIG_FILE/.s3.tfbackend/}"
-ACCOUNT_ID="${BACKEND_CONFIG_NAME/$ACCOUNT_NAME./}"
-echo -n "$ACCOUNT_ID"
+BACKEND_CONFIG_FILE_GLOB="$SCRIPT_DIR"/../infra/accounts/"$ACCOUNT_NAME".*.s3.tfbackend
+if [ ! -e $BACKEND_CONFIG_FILE_GLOB ]; then
+  echo "null"
+else
+  BACKEND_CONFIG_FILE_PATH=$(ls -1 $BACKEND_CONFIG_FILE_GLOB)
+  BACKEND_CONFIG_FILE=$(basename "$BACKEND_CONFIG_FILE_PATH")
+  BACKEND_CONFIG_NAME="${BACKEND_CONFIG_FILE/.s3.tfbackend/}"
+  ACCOUNT_ID="${BACKEND_CONFIG_NAME/$ACCOUNT_NAME./}"
+  echo -n "{\"account_id\": \"$ACCOUNT_ID\"}"
+fi

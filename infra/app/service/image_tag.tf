38fdbd6758cc870890cd7ec31ec5b94b029bd171
Loren Yu <loren@navapbc.com>
2023-09-05 12:48:29 -0700

    Move local vars up

--- infra/app/service/image_tag.tf
+++ infra/app/service/image_tag.tf
@@ -6,6 +6,15 @@
 #  1. Accept an optional variable during a terraform plan/apply. (see "image_tag" variable in variables.tf)
 
 #  2. Read the output used from the last terraform state using "terraform_remote_state".
+#     Get the backend config by parsing the backend config file
+locals {
+  backend_config_file_path = "${path.module}/${var.environment_name}.s3.tfbackend"
+  backend_config_file      = file("${path.module}/${var.environment_name}.s3.tfbackend")
+  backend_config_regex     = "(\\w+)\\s*=\\s*\\\"(\\w+)\\\""
+  backend_config           = { for match in regexall("(\\w+)\\s*=\\s*\\\"(.+)\\\"", local.backend_config_file) : match[0] => match[1] }
+  tfstate_bucket           = local.backend_config["bucket"]
+  tfstate_key              = local.backend_config["key"]
+}
 data "terraform_remote_state" "current_image_tag" {
   # Don't do a lookup if image_tag is provided explicitly.
   # This saves some time and also allows us to do a first deploy,
@@ -26,12 +35,6 @@ data "terraform_remote_state" "current_image_tag" {
 
 #  3. Prefer the given variable if provided, otherwise default to the value from last time.
 locals {
-  backend_config_file_path = "${path.module}/${var.environment_name}.s3.tfbackend"
-  backend_config_file      = file("${path.module}/${var.environment_name}.s3.tfbackend")
-  backend_config_regex     = "(\\w+)\\s*=\\s*\\\"(\\w+)\\\""
-  backend_config           = { for match in regexall("(\\w+)\\s*=\\s*\\\"(.+)\\\"", local.backend_config_file) : match[0] => match[1] }
-  tfstate_bucket           = local.backend_config["bucket"]
-  tfstate_key              = local.backend_config["key"]
   image_tag = (var.image_tag == null
     ? data.terraform_remote_state.current_image_tag[0].outputs.image_tag
   : var.image_tag)

4e7ee207a61578749e2449380c1b6660fcdeef55
FJOR <charlesfraley@navapbc.com>
2023-09-12 10:25:23 -0700

    Remove reference to old policy
    #354
    Dropped data resource which referenced now deleted policy

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -67,11 +67,6 @@ data "aws_rds_cluster" "db_cluster" {
   cluster_identifier = local.database_config.cluster_name
 }
 
-data "aws_iam_policy" "db_access_policy" {
-  count = module.app_config.has_database ? 1 : 0
-  name  = local.database_config.access_policy_name
-}
-
 data "aws_iam_policy" "app_db_access_policy" {
   count = module.app_config.has_database ? 1 : 0
   name  = local.database_config.app_access_policy_name

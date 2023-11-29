e89591c7435d3ab238a6832e192756cbb73f3343
Loren Yu <loren@navapbc.com>
2023-11-29 14:27:56 -0800

    Export name of feature flag project

--- infra/modules/feature-flagging/outputs.tf
+++ infra/modules/feature-flagging/outputs.tf
@@ -1,3 +1,8 @@
+output "name" {
+  description = "Name of feature flag manager"
+  value       = local.evidently_project_name
+}
+
 output "access_policy_arn" {
   description = "Policy that allows access to query feature flag values"
   value       = data.aws_iam_policy.access_policy.arn

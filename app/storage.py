07ca5734b279158fc56887121d36062ae5f6e728
Loren Yu <loren@navapbc.com>
2023-12-13 17:10:48 -0800

    Use siganture version 4

--- app/storage.py
+++ app/storage.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import boto3
+from botocore.config import Config
 
 logger = logging.getLogger()
 
@@ -9,7 +10,13 @@ logger = logging.getLogger()
 def create_upload_url(path):
     bucket_name = os.environ.get("BUCKET_NAME")
 
-    s3_client = boto3.client("s3")
+    s3_client = boto3.client(
+        "s3",
+        config=Config(
+            # By default presigned URLs use signature version 2
+            signature_version="v4",
+        ),
+    )
     logger.info("Generating presigned POST URL")
     response = s3_client.generate_presigned_post(bucket_name, path)
     return response["url"], response["fields"]

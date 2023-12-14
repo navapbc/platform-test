9e855fa03d181ac49a5dd973d954de7799d2f1b0
Loren Yu <loren@navapbc.com>
2023-12-13 17:13:38 -0800

    Use boto3 method

--- app/storage.py
+++ app/storage.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 import boto3
-from botocore.config import Config
+from botocore.client import Config
 
 logger = logging.getLogger()
 
@@ -10,13 +10,9 @@ logger = logging.getLogger()
 def create_upload_url(path):
     bucket_name = os.environ.get("BUCKET_NAME")
 
-    s3_client = boto3.client(
-        "s3",
-        config=Config(
-            # By default presigned URLs use signature version 2
-            signature_version="v4",
-        ),
-    )
+    # Manually specify signature version 4 which is required since the bucket is encrypted with KMS.
+    # By default presigned URLs use signature version 2 to be backwards compatible
+    s3_client = boto3.client("s3", config=Config(signature_version="s3v4"))
     logger.info("Generating presigned POST URL")
     response = s3_client.generate_presigned_post(bucket_name, path)
     return response["url"], response["fields"]

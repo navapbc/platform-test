c054d350fb046250760bec38e632c1e8cf401490
Loren Yu <loren@navapbc.com>
2024-02-22 11:31:15 -0800

    Ignore default KMS key checkov issue

--- infra/modules/domain/query-logs.tf
+++ infra/modules/domain/query-logs.tf
@@ -5,6 +5,8 @@ resource "aws_cloudwatch_log_group" "dns_query_logging" {
 
   name              = "/dns/${var.name}"
   retention_in_days = 30
+
+  # checkov:skip=CKV_AWS_158:No need to manage KMS key for DNS query logs or audit access to these logs
 }
 
 resource "aws_route53_query_log" "dns_query_logging" {

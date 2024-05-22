db8eed6b59b64f9bb52cbc9c85573d99a8a77f5c
Loren Yu <loren@navapbc.com>
2024-05-21 20:35:44 -0700

    Only append workspace to generated secret store paths

--- infra/app/service/secrets.tf
+++ infra/app/service/secrets.tf
@@ -3,9 +3,12 @@ module "secrets" {
 
   source = "../../modules/secret"
 
-  # Append the terraform workspace to the secret store path if the environment is temporary
-  # to avoid conflicts with existing environments
-  secret_store_path = (local.is_temporary ?
+  # When generating secrets and storing them in parameter store, append the
+  # terraform workspace to the secret store path if the environment is temporary
+  # to avoid conflicts with existing environments.
+  # Don't do this for secrets that are managed manually since the temporary
+  # environments will need to share those secrets.
+  secret_store_path = (each.value.manage_method == "code" && local.is_temporary ?
     "${each.value.secret_store_path}/${terraform.workspace}" :
     each.value.secret_store_path
   )

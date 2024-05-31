44d055b01440d5426c73195c9297fa98bf5d018a
Loren Yu <loren@navapbc.com>
2024-05-31 12:55:09 -0700

    Fix module uninitialized

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -5,9 +5,6 @@ APP_NAME=$1
 IMAGE_TAG=$2
 ENVIRONMENT=$3
 
-CLUSTER_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_cluster_name)
-SERVICE_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_name)
-
 echo "--------------"
 echo "Deploy release"
 echo "--------------"
@@ -15,16 +12,17 @@ echo "Input parameters:"
 echo "  APP_NAME=$APP_NAME"
 echo "  IMAGE_TAG=$IMAGE_TAG"
 echo "  ENVIRONMENT=$ENVIRONMENT"
-echo "Additional context:"
-echo "  CLUSTER_NAME=$CLUSTER_NAME"
-echo "  SERVICE_NAME=$SERVICE_NAME"
 echo
 echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
 
+# Update task definition and update service to use new task definition
 
 TF_CLI_ARGS_apply="-input=false -auto-approve -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT"
 
 # Wait for the service to become stable
+
+CLUSTER_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_cluster_name)
+SERVICE_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_name)
 echo "Waiting for service $SERVICE_NAME to become stable"
 aws ecs wait services-stable --cluster "$CLUSTER_NAME" --services "$SERVICE_NAME"
 

63df1fcc6da2dfcb056961f8d62c8c9cb7b74081
Loren Yu <loren@navapbc.com>
2024-05-31 13:01:58 -0700

    Group logs

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -17,13 +17,15 @@ echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
 
 # Update task definition and update service to use new task definition
 
+echo "::group::Update task definition and update service to use new task definition"
 TF_CLI_ARGS_apply="-input=false -auto-approve -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT"
+echo "::endgroup::"
 
 # Wait for the service to become stable
 
 CLUSTER_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_cluster_name)
 SERVICE_NAME=$(terraform -chdir="infra/$APP_NAME/service" output -raw service_name)
-echo "Waiting for service $SERVICE_NAME to become stable"
+echo "Wait for service $SERVICE_NAME to become stable"
 aws ecs wait services-stable --cluster "$CLUSTER_NAME" --services "$SERVICE_NAME"
 
 echo "Completed $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"

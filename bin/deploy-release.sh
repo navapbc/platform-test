c7712cb1b41463d09d352715241bb6b6bed2b45f
Loren Yu <loren@navapbc.com>
2024-05-31 13:08:36 -0700

    Consolidate duplicate logs

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -13,11 +13,10 @@ echo "  APP_NAME=$APP_NAME"
 echo "  IMAGE_TAG=$IMAGE_TAG"
 echo "  ENVIRONMENT=$ENVIRONMENT"
 echo
-echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
 
 # Update task definition and update service to use new task definition
 
-echo "::group::Update task definition and update service to use new task definition"
+echo "::group::Starting $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
 TF_CLI_ARGS_apply="-input=false -auto-approve -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT"
 echo "::endgroup::"
 

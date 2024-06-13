6946ead48db5780ca26507eff682c5b37bcee31a
Daphne Gold <daphnegold@navapbc.com>
2024-06-13 13:08:02 -0700

    Remove old .sh files

--- bin/current-account-alias.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Print the current AWS account alias
-set -euo pipefail
-echo -n "$(aws iam list-account-aliases --query "AccountAliases" --max-items 1 --output text)"
--- bin/current-account-id.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Print the current AWS account id
-set -euo pipefail
-echo -n "$(aws sts get-caller-identity --query "Account" --output text)"

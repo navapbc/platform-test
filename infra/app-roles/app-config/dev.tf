adf7fdb63148f9597a003ffc0518aba20a3099df
Rocket <rocketnova@users.noreply.github.com>
2024-06-14 16:08:39 -0700

    Add test application

---
+++ infra/app-roles/app-config/dev.tf
@@ -0,0 +1,18 @@
+module "dev_config" {
+  source                          = "./env-config"
+  project_name                    = local.project_name
+  app_name                        = local.app_name
+  default_region                  = module.project_config.default_region
+  environment                     = "dev"
+  account_name                    = "dev"
+  network_name                    = "dev"
+  domain_name                     = "platform-test-dev.navateam.com"
+  enable_https                    = true
+  has_database                    = local.has_database
+  has_incident_management_service = local.has_incident_management_service
+
+  # Enables ECS Exec access for debugging or jump access.
+  # See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-exec.html
+  # Defaults to `false`. Uncomment the next line to enable.
+  # enable_command_execution = true
+}

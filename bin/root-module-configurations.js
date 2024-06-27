a408b7f2fed8d855a28bfd3f18d6ee700c8cc167
Loren Yu <loren@navapbc.com>
2024-06-27 11:04:13 -0700

    Add account_name to configs

--- bin/root-module-configurations.js
+++ bin/root-module-configurations.js
@@ -3,22 +3,62 @@
 // [["rootModule": "path/to/root/module", "backendConfigName": "dev"], ...]
 const fs = require('fs');
 const path = require('path');
-
-let rootModuleConfigs = [];
+const { execSync } = require('child_process');
 
 function main() {
-  // Iterate over infra layers
-  for (const infraLayer of ["account", "network"]) {
-    rootModuleConfigs.push(...getRootModuleConfigs(`${infraLayer}s`))
-  }
+  let rootModuleConfigs = [];
+
+  // Account layer
+  rootModuleConfigs.push(...getAccountLayerConfigs())
+
+  // Network layer
+  rootModuleConfigs.push(...getNetworkLayerConfigs())
 
   for (const appName of getAppNames()) {
-    for (const infraLayer of ["build-repository", "database", "service"]) {
-      rootModuleConfigs.push(...getRootModuleConfigs(path.join(appName, infraLayer)))
-    }
+    console.log(`Processing app: ${appName}`)
+    rootModuleConfigs.push(...getAppConfigs(appName))
   }
 
   console.log(JSON.stringify(rootModuleConfigs));
+}
+
+function getAccountLayerConfigs() {
+  accountLayerConfigs = getRootModuleConfigs("accounts")
+  accountLayerConfigs.forEach(config => {
+    config.account_name = config.backend_config_name.split(".")[0]
+  })
+  return accountLayerConfigs
+}
+
+function getNetworkLayerConfigs() {
+  execSync("terraform -chdir=infra/project-config init")
+  execSync("terraform -chdir=infra/project-config apply -auto-approve")
+  const command = "terraform -chdir=infra/project-config output -json network_configs";
+  const output = execSync(command, { encoding: 'utf8' });
+  const networkConfigs = JSON.parse(output);
+ 
+  networkLayerConfigs = getRootModuleConfigs("networks")
+  networkLayerConfigs.forEach(config => {
+    config.account_name = networkConfigs[config.backend_config_name].account_name
+  })
+  return networkLayerConfigs
+}
+
+function getAppConfigs(appName) {
+  execSync(`terraform -chdir=infra/${appName}/app-config init`)
+  execSync(`terraform -chdir=infra/${appName}/app-config apply -auto-approve`)
+  const command = `terraform -chdir=infra/${appName}/app-config output -json account_names_by_environment`;
+  const output = execSync(command, { encoding: 'utf8' });
+  const accountNamesByEnvironment = JSON.parse(output);
+
+  const rootModuleConfigs = []
+  for (const infraLayer of ["build-repository", "database", "service"]) {
+    rootModuleConfigs.push(...getRootModuleConfigs(path.join(appName, infraLayer)))
+  }
+  rootModuleConfigs.forEach(config => {
+    config.account_name = accountNamesByEnvironment[config.backend_config_name]
+  })
+  return rootModuleConfigs
 }
 
 function getRootModuleConfigs(rootModuleSubdir) {

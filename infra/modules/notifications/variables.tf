54a31a949eaa53a6e3a03938438cc23da8c8a7d3
Rocket <rocketnova@users.noreply.github.com>
2024-08-08 15:36:33 -0700

    Add code for aws_sesv2_email_identity_policy

--- infra/modules/notifications/variables.tf
+++ infra/modules/notifications/variables.tf
@@ -1,10 +1,10 @@
 variable "email_configuration_set_name" {
-  type = string
+  type        = string
   description = "The name of the SESv2 configuration set to apply to the pinpoint email channel"
 }
 
 variable "email_identity_arn" {
-  type = string
+  type        = string
   description = "The arn for the SESv2 email identity to use to send emails"
 }
 

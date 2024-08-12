5dbcda6a20235dd77a2b163d5737b2b8315bf114
Rocket <rocketnova@users.noreply.github.com>
2024-08-12 15:07:40 -0700

    Remove deprecated pinpoint identity role

--- infra/modules/notifications/email.tf
+++ infra/modules/notifications/email.tf
@@ -9,10 +9,4 @@ resource "aws_pinpoint_email_channel" "email" {
   configuration_set = var.email_configuration_set_name
   from_address      = var.sender_email != null ? (var.sender_display_name != null ? "${var.sender_display_name} <${var.sender_email}>" : var.sender_email) : null
   identity          = var.email_identity_arn
-
-  # Note: There is a known bug where role_arn isn't being persisted to AWS. This means
-  # that this module will always show that there are changes that haven't been applied.
-  # Commenting it out for now.
-  # See https://github.com/hashicorp/terraform-provider-aws/issues/38772
-  # role_arn          = aws_iam_role.analytics.arn
 }

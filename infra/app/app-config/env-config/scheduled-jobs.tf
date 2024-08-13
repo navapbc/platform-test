2c00c2f53ae1630d84e1b78427e12f54446dfcc8
Kai Siren <kaisiren@navapbc.com>
2024-08-13 16:41:20 -0700

    checkpoint-kai-1723592480

--- infra/app/app-config/env-config/scheduled-jobs.tf
+++ infra/app/app-config/env-config/scheduled-jobs.tf
@@ -1,4 +1,5 @@
 locals {
+  # See `infra/modules/service/variables.tf` for the format of the scheduled_jobs object
   scheduled_jobs = [
     {
       name                         = "${var.app_name}-${var.environment}-show-files"

15f623a31c3d60e5d9980bba9ed2eddf7f90bdf3
Kai Siren <kaisiren@navapbc.com>
2024-09-13 10:59:14 -0700

    states array

--- infra/modules/service/events_jobs.tf
+++ infra/modules/service/events_jobs.tf
@@ -120,7 +120,7 @@ resource "aws_sfn_state_machine" "file_upload_jobs" {
             "ContainerOverrides" : [
               {
                 "Name" : local.container_name,
-                "Command" : "$.containerOverrides[0].command"
+                "Command" : "States.Array($.containerOverrides[0].command)"
               }
             ]
           }

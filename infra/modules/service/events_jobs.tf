d832a10c978a51c8d797b2807e53ebb94aa0f780
Kai Siren <kaisiren@navapbc.com>
2024-09-13 08:43:16 -0700

    try using detail for command

--- infra/modules/service/events_jobs.tf
+++ infra/modules/service/events_jobs.tf
@@ -42,6 +42,55 @@ resource "aws_cloudwatch_event_target" "document_upload_jobs" {
   rule      = aws_cloudwatch_event_rule.file_upload_jobs[each.key].name
   arn       = aws_sfn_state_machine.file_upload_jobs[each.key].arn
   role_arn  = aws_iam_role.events.arn
+
+  input_transformer {
+    input_paths = {
+      bucket_name = "$.detail.bucket.name",
+      object_key  = "$.detail.object.key",
+    }
+
+    # When triggering the ECS task, override the command to run in the container to the
+    # command specified by the file_upload_job config. To do this define an input_template
+    # that transforms the input S3 event:
+    #   {
+    #     detail: {
+    #       bucket: { name: "mybucket" },
+    #       object: { key: "uploaded/file/path" }
+    #     }
+    #   }
+    # to match the Amazon ECS RunTask TaskOverride structure:
+    #   {
+    #     containerOverrides: [{
+    #       name: "container_name",
+    #       command: ["command", "to", "run"]
+    #     }]
+    #   }
+    # (see https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-targets.html#targets-specifics-ecs-task
+    # and https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_TaskOverride.html)
+    #
+    # The task command can optionally use the bucket name or the object key in the command
+    # by including the placeholder values "<bucket_name>" or "<object_key>", e.g.
+    #   {
+    #     containerOverrides: [{
+    #       name: "container_name",
+    #       command: ["process_file.sh", "--bucket", "<bucket_name>", "--object", "<object_key>"]
+    #     }]
+    #   }
+    #
+    # Since jsonencode will cause the string "<bucket_name>" to turn into
+    # "U+003Cbucket_nameU+003E" and "<object_key>" to turn into "U+003Cobject_keyU+003E",
+    # we need to replace the unicode characters U+003C and U+003E with < and > to reverse
+    # the encoding.
+    # (see https://developer.hashicorp.com/terraform/language/functions/jsonencode and
+    # https://github.com/hashicorp/terraform/pull/18871)
+    input_template = replace(replace(jsonencode({
+      containerOverrides = [
+        {
+          command = each.value.task_command
+        }
+      ]
+    }), "\\u003c", "<"), "\\u003e", ">")
+  }
 }
 
 resource "aws_sfn_state_machine" "file_upload_jobs" {
@@ -71,7 +120,7 @@ resource "aws_sfn_state_machine" "file_upload_jobs" {
             "ContainerOverrides" : [
               {
                 "Name" : local.container_name,
-                "Command" : each.value.task_command
+                "Command" : "$.detail.command"
               }
             ]
           }

be4e5926e000ae66bf5463e557349dbc7257a71f
Kai Siren <kaisiren@navapbc.com>
2024-09-13 05:36:07 -0700

    run s3 jobs for step functions

--- infra/modules/service/events_jobs.tf
+++ infra/modules/service/events_jobs.tf
@@ -40,68 +40,67 @@ resource "aws_cloudwatch_event_target" "document_upload_jobs" {
 
   target_id = "${local.cluster_name}-${each.key}"
   rule      = aws_cloudwatch_event_rule.file_upload_jobs[each.key].name
-  arn       = aws_ecs_cluster.cluster.arn
+  arn       = aws_sfn_state_machine.file_upload_jobs[each.key].arn
   role_arn  = aws_iam_role.events.arn
+}
 
-  ecs_target {
-    task_definition_arn = aws_ecs_task_definition.app.arn
-    launch_type         = "FARGATE"
-    propagate_tags      = "TASK_DEFINITION"
+resource "aws_sfn_state_machine" "file_upload_jobs" {
+  for_each = var.file_upload_jobs
 
-    # Configuring Network Configuration is required when the task definition uses the awsvpc network mode.
-    network_configuration {
-      subnets         = var.private_subnet_ids
-      security_groups = [aws_security_group.app.id]
+  name     = "${var.service_name}-${each.key}"
+  role_arn = aws_iam_role.workflow_orchestrator.arn
+
+  definition = jsonencode({
+    "StartAt" : "RunTask",
+    "States" : {
+      "RunTask" : {
+        "Type" : "Task",
+        # docs: https://docs.aws.amazon.com/step-functions/latest/dg/connect-ecs.html
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "Parameters" : {
+          "Cluster" : aws_ecs_cluster.cluster.arn,
+          "TaskDefinition" : aws_ecs_task_definition.app.arn,
+          "LaunchType" : "FARGATE",
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : var.private_subnet_ids,
+              "SecurityGroups" : [aws_security_group.app.id],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : local.container_name,
+                "Command" : each.value.task_command
+              }
+            ]
+          }
+        },
+        "End" : true
+      }
     }
+  })
+
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.file_upload_jobs[each.key].arn}:*"
+    include_execution_data = true
+    level                  = "ERROR"
   }
 
-  input_transformer {
-    input_paths = {
-      bucket_name = "$.detail.bucket.name",
-      object_key  = "$.detail.object.key",
-    }
-
-    # When triggering the ECS task, override the command to run in the container to the
-    # command specified by the file_upload_job config. To do this define an input_template
-    # that transforms the input S3 event:
-    #   {
-    #     detail: {
-    #       bucket: { name: "mybucket" },
-    #       object: { key: "uploaded/file/path" }
-    #     }
-    #   }
-    # to match the Amazon ECS RunTask TaskOverride structure:
-    #   {
-    #     containerOverrides: [{
-    #       name: "container_name",
-    #       command: ["command", "to", "run"]
-    #     }]
-    #   }
-    # (see https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-targets.html#targets-specifics-ecs-task
-    # and https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_TaskOverride.html)
-    #
-    # The task command can optionally use the bucket name or the object key in the command
-    # by including the placeholder values "<bucket_name>" or "<object_key>", e.g.
-    #   {
-    #     containerOverrides: [{
-    #       name: "container_name",
-    #       command: ["process_file.sh", "--bucket", "<bucket_name>", "--object", "<object_key>"]
-    #     }]
-    #   }
-    #
-    # Since jsonencode will cause the string "<bucket_name>" to turn into
-    # "U+003Cbucket_nameU+003E" and "<object_key>" to turn into "U+003Cobject_keyU+003E",
-    # we need to replace the unicode characters U+003C and U+003E with < and > to reverse
-    # the encoding.
-    # (see https://developer.hashicorp.com/terraform/language/functions/jsonencode and
-    # https://github.com/hashicorp/terraform/pull/18871)
-    input_template = replace(replace(jsonencode({
-      containerOverrides = [
-        {
-          name    = local.container_name,
-          command = each.value.task_command
-        }
-      ]
-    }), "\\u003c", "<"), "\\u003e", ">")
+  tracing_configuration {
+    enabled = true
   }
+}
+
+resource "aws_cloudwatch_log_group" "file_upload_jobs" {
+  for_each = var.file_upload_jobs
+
+  name_prefix = "/aws/vendedlogs/states/${var.service_name}-${each.key}"
+
+  # Conservatively retain logs for 5 years.
+  # Looser requirements may allow shorter retention periods
+  retention_in_days = 1827
+
+  # TODO(https://github.com/navapbc/template-infra/issues/164) Encrypt with customer managed KMS key
+  # checkov:skip=CKV_AWS_158:Encrypt service logs with customer key in future work
 }

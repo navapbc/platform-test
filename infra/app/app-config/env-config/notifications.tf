044b13c6dfe4defc6eea59c0f82a6d692dec7a81
Kai Siren <kaisiren@navapbc.com>
2024-10-07 10:24:48 -0700

    trigger ci

--- infra/app/app-config/env-config/notifications.tf
+++ infra/app/app-config/env-config/notifications.tf
@@ -15,14 +15,14 @@ locals {
 
     # Configure the name that users see in the "From" section of their inbox, so that it's
     # clearer who the email is from.
-    sender_display_name = "FOOBAR"
+    sender_display_name = "coilysiren"
 
     # Set to the email address to be used when sending emails.
     # - If enable_notifications is true, this is required.
     # - If email_verification_method is set to 'domain', make sure the domain name of the
     #   sender_email matches the domain provided in the env-config.
     # Docs: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-email.html
-    sender_email = "rocket@navateam.com"
+    sender_email = "kaisiren@navapbc.com"
 
     # Configure the REPLY-TO email address if it should be different from the sender.
     # Note: Only used by the identity-provider service.

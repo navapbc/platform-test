33e654dba5514f61ca52fc3f15239bda9f2930e5
Kai Siren <kaisiren@navapbc.com>
2024-10-07 10:07:01 -0700

    use route53 config from truss

--- infra/modules/email-identity/main.tf
+++ infra/modules/email-identity/main.tf
@@ -3,15 +3,12 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  # Construct DNS records to be added to the sending domain.
-  # Only used if the sender identity verification method is domain verification.
-  dkim_dns_verification_records = var.email_verification_method == "domain" ? [
-    for token in aws_sesv2_email_identity.sender.dkim_signing_attributes[0].tokens : {
-      type   = "CNAME"
-      name   = "${token}._domainkey"
-      record = "${token}.dkim.amazonses.com"
-    }
-  ] : []
+  # some ses resources don't allow for the terminating '.' in the domain name
+  # so use a replace function to strip it out
+  stripped_domain_name = replace(var.sender_email_domain_name, "/[.]$/", "")
+
+  stripped_mail_from_domain = replace(var.sender_email, "/[.]$/", "")
+  dash_domain               = replace(var.sender_email_domain_name, ".", "-")
 }
 
 # Verify email sender identity.
@@ -83,13 +80,55 @@ resource "aws_route53_zone" "zone" {
 }
 
 # DNS records for email identity verification if email_verification_method is "domain"
-resource "aws_route53_record" "verification0" {
-  count = length(local.dkim_dns_verification_records)
+resource "aws_route53_record" "dkim" {
+  count = var.email_verification_method == "domain" ? 3 : 0
 
   allow_overwrite = true
-  zone_id         = aws_route53_zone.zone[count.index].zone_id
-  name            = local.dkim_dns_verification_records[count.index].name
-  type            = local.dkim_dns_verification_records[count.index].type
-  records         = [local.dkim_dns_verification_records[count.index].record]
   ttl             = 60
+  type            = "CNAME"
+  zone_id         = aws_route53_zone.zone.zone_id
+  name            = "${aws_sesv2_email_identity.sender.dkim_signing_attributes[0].tokens[count.index]}._domainkey"
+  records         = ["${aws_sesv2_email_identity.sender.dkim_signing_attributes[0].tokens[count.index]}.dkim.amazonses.com"]
+
+  depends_on = [aws_sesv2_email_identity.sender]
+}
+
+resource "aws_sesv2_email_identity_mail_from_attributes" "main" {
+  email_identity   = aws_sesv2_email_identity.main.email_identity
+  mail_from_domain = local.stripped_mail_from_domain
+
+  depends_on = [aws_sesv2_email_identity.main]
+}
+
+resource "aws_route53_record" "spf_mail_from" {
+  count = var.email_verification_method == "domain" ? 1 : 0
+
+  allow_overwrite = true
+  ttl             = "600"
+  type            = "TXT"
+  zone_id         = aws_route53_zone.zone.zone_id
+  name            = aws_sesv2_email_identity_mail_from_attributes.main.mail_from_domain
+  records         = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_route53_record" "mx_send_mail_from" {
+  count = var.email_verification_method == "domain" ? 1 : 0
+
+  allow_overwrite = true
+  type            = "MX"
+  ttl             = "600"
+  zone_id         = aws_route53_zone.zone.zone_id
+  name            = aws_sesv2_email_identity_mail_from_attributes.main.mail_from_domain
+  records         = ["10 feedback-smtp.${data.aws_region.current.name}.amazonses.com"]
+}
+
+resource "aws_route53_record" "mx_receive" {
+  count = var.email_verification_method == "domain" ? 1 : 0
+
+  allow_overwrite = true
+  type            = "MX"
+  ttl             = "600"
+  name            = var.sender_email_domain_name
+  zone_id         = aws_route53_zone.zone.zone_id
+  records         = ["10 inbound-smtp.${data.aws_region.current.name}.amazonaws.com"]
 }

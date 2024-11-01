d8ecbfe8c48498bd316c5813cd85f13598e5aa04
Kai Siren <kaisiren@navapbc.com>
2024-11-01 11:51:17 -0700

    Revert "swap to real versions"
    This reverts commit cc8e67907a1477ead748baa5cfbf5edf0451f608.

--- infra/app/service/notifications.tf
+++ infra/app/service/notifications.tf
@@ -1,10 +1,16 @@
 locals {
   # If this is a temporary environment, re-use an existing email identity. Otherwise, create a new one.
+  # email_identity_arn = module.app_config.enable_notifications ? (
+  #   !local.is_temporary ? module.notifications[0].email_identity_arn : module.existing_notifications[0].email_identity_arn
+  # ) : null
   email_identity_arn = module.app_config.enable_notifications ? (
-    !local.is_temporary ? module.notifications[0].email_identity_arn : module.existing_notifications[0].email_identity_arn
+    terraform.workspace == "p-140" ? module.notifications[0].email_identity_arn : module.existing_notifications[0].email_identity_arn
   ) : null
+  # email_identity_config = module.app_config.enable_notifications ? (
+  #   !local.is_temporary ? module.notifications[0].email_identity_config : module.existing_notifications[0].email_identity_config
+  # ) : null
   email_identity_config = module.app_config.enable_notifications ? (
-    !local.is_temporary ? module.notifications[0].email_identity_config : module.existing_notifications[0].email_identity_config
+    terraform.workspace == "p-140" ? module.notifications[0].email_identity_config : module.existing_notifications[0].email_identity_config
   ) : null
   notifications_environment_variables = module.app_config.enable_notifications ? {
     PINPOINT_APP_ID = module.notifications_app[0].app_id,
@@ -15,7 +21,8 @@ locals {
 # If the app has `enable_notifications` set to true AND this is not a temporary
 # environment, then create a email notification identity.
 module "notifications" {
-  count  = module.app_config.enable_notifications && !local.is_temporary ? 1 : 0
+  # count  = module.app_config.enable_notifications && !local.is_temporary ? 1 : 0
+  count  = terraform.workspace == "p-140" ? 1 : 0 # test in particular PR environment
   source = "../../modules/notifications/resources"
 
   domain_name         = local.service_config.domain_name
@@ -27,7 +34,8 @@ module "notifications" {
 # If the app has `enable_notifications` set to true AND this *is* a temporary
 # environment, then create a email notification identity.
 module "existing_notifications" {
-  count  = module.app_config.enable_notifications && local.is_temporary ? 1 : 0
+  # count  = module.app_config.enable_notifications && local.is_temporary ? 1 : 0
+  count  = terraform.workspace != "p-140" ? 1 : 0 # test in particular PR environment
   source = "../../modules/notifications/data"
 
   name        = local.notifications_config.name

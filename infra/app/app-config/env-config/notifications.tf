bd6e8350890909ede6029d9241f954ea3bd219fc
Kai Siren <kaisiren@navapbc.com>
2024-11-01 14:30:05 -0700

    auto code review

--- infra/app/app-config/env-config/notifications.tf
+++ infra/app/app-config/env-config/notifications.tf
@@ -4,28 +4,15 @@ locals {
     # Pinpoint app name.
     name = "${var.app_name}-${var.environment}"
 
-    # The method to use to verify the sender email address.
-    # - Must be 'email' or 'domain'.
-    # - If method is 'email', AWS will send you an email with a one-time link. Click on
-    #   the link to verify the email address.
-    # - If method is 'domain', then custom domains are required and the domain used will
-    #   match the one set in the env-config. See /docs/infra/set-up-custom-domains.md
-    # Docs: https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-email-manage-verify.html
-    email_verification_method = "domain"
-
-    # Configure the name that users see in the "From" section of their inbox, so that it's
-    # clearer who the email is from.
+    # Configure the name that users see in the "From" section of their inbox,
+    # so that it's clearer who the email is from.
     sender_display_name = "Kai Siren"
 
     # Set to the email address to be used when sending emails.
-    # - If enable_notifications is true, this is required.
-    # - If email_verification_method is set to 'domain', make sure the domain name of the
-    #   sender_email matches the domain provided in the env-config.
-    # Docs: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-email.html
-    sender_email = "coilysiren@platform-test-dev.navapbc.com"
+    # If enable_notifications is true, this is required.
+    sender_email = "coilysiren@${domain_name}"
 
     # Configure the REPLY-TO email address if it should be different from the sender.
-    # Note: Only used by the identity-provider service.
-    reply_to_email = "coilysiren@platform-test-dev.navapbc.com"
+    reply_to_email = "coilysiren@${domain_name}"
   } : null
 }
--- infra/modules/notifications-app/resources/main.tf
+++ infra/modules/notifications-app/resources/main.tf
@@ -1,14 +1,7 @@
-# Create the AWS Pinpoint application.
 resource "aws_pinpoint_app" "app" {
   name = var.name
 }
 
-# The AWS Pinpoint Email channel requires sender identity verification. It supports:
-# 1. email address verification: Verify an email address. An email is sent to the email
-#    address with a link to verify you own the email address.
-# 2. domain verification: Verify an entire domain. When you verify a new domain, AWS
-#    provides a set of DNS records. You have to add these records to the DNS configuration
-#    of the domain.
 resource "aws_pinpoint_email_channel" "app" {
   application_id    = aws_pinpoint_app.app.application_id
   configuration_set = var.email_identity_config

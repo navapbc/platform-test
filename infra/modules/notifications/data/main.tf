52643a53ebc02c639fd8ef235cf724e0b9e0a75a
Kai Siren <kaisiren@navapbc.com>
2024-11-08 08:14:18 -0800

    cleanup some vars

--- infra/modules/notifications/data/main.tf
+++ infra/modules/notifications/data/main.tf
@@ -1,9 +1,5 @@
-locals {
-  stripped_domain_name = replace(var.domain_name, "/[.]$/", "")
-}
-
 data "aws_sesv2_email_identity" "main" {
-  email_identity = local.stripped_domain_name
+  email_identity = var.domain_name
 }
 
 data "aws_sesv2_configuration_set" "main" {

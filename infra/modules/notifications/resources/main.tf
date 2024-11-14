fe2678dc2a375c84da8ee9b264af4a805f300e4b
Kai Siren <kaisiren@navapbc.com>
2024-11-14 01:12:19 -0800

    reorg

--- infra/modules/notifications/resources/main.tf
+++ infra/modules/notifications/resources/main.tf
@@ -1,9 +1,3 @@
 resource "aws_pinpoint_app" "app" {
   name = var.name
 }
-
-resource "aws_pinpoint_email_channel" "app" {
-  application_id = aws_pinpoint_app.app.application_id
-  from_address   = var.sender_email != null ? (var.sender_display_name != null ? "${var.sender_display_name} <${var.sender_email}>" : var.sender_email) : null
-  identity       = var.domain_identity_arn
-}

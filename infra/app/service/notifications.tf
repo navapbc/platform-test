31eb7b2a5e6c054265dfb091a852b29f88ee77bb
Kai Siren <kaisiren@navapbc.com>
2024-11-14 00:04:18 -0800

    delete it please, thanks

--- infra/app/service/notifications.tf
+++ infra/app/service/notifications.tf
@@ -1,13 +1,13 @@
 locals {
-  # If this is a temporary environment, re-use an existing email identity. Otherwise, create a new one.
-  domain_identity_arn = module.app_config.enable_notifications ? (
-    !local.is_temporary ? module.notifications[0].domain_identity_arn : module.existing_notifications[0].domain_identity_arn
-  ) : null
-  notifications_environment_variables = module.app_config.enable_notifications ? {
-    AWS_PINPOINT_APP_ID       = module.notifications_app[0].app_id,
-    AWS_PINPOINT_SENDER_EMAIL = local.notifications_config.sender_email
-  } : {}
-  notifications_app_name = "${local.prefix}${local.notifications_config.name}"
+  # # If this is a temporary environment, re-use an existing email identity. Otherwise, create a new one.
+  # domain_identity_arn = module.app_config.enable_notifications ? (
+  #   !local.is_temporary ? module.notifications[0].domain_identity_arn : module.existing_notifications[0].domain_identity_arn
+  # ) : null
+  # notifications_environment_variables = module.app_config.enable_notifications ? {
+  #   AWS_PINPOINT_APP_ID       = module.notifications_app[0].app_id,
+  #   AWS_PINPOINT_SENDER_EMAIL = local.notifications_config.sender_email
+  # } : {}
+  # notifications_app_name = "${local.prefix}${local.notifications_config.name}"
 }
 
 # If the app has `enable_notifications` set to true AND this is not a temporary

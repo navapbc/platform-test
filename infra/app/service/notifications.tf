103f386486a93d926bf8ceaa7dc6a881ac2f1e07
Kai Siren <kaisiren@navapbc.com>
2024-11-14 01:08:35 -0800

    update vars

--- infra/app/service/notifications.tf
+++ infra/app/service/notifications.tf
@@ -1,7 +1,9 @@
 locals {
   # If this is a temporary environment, re-use an existing email identity. Otherwise, create a new one.
   domain_identity_arn = module.app_config.enable_notifications ? (
-    !local.is_temporary ? module.notifications[0].domain_identity_arn : module.existing_notifications[0].domain_identity_arn
+    !local.is_temporary ?
+    module.notifications_email_domain[0].domain_identity_arn :
+    module.existing_notifications_email_domain[0].domain_identity_arn
   ) : null
   notifications_environment_variables = module.app_config.enable_notifications ? {
     AWS_PINPOINT_APP_ID       = module.notifications_app[0].app_id,

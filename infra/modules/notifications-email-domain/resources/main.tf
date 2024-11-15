8a8a6e93b179f47524f6b0ca80e370f595c0235e
Kai Siren <kaisiren@navapbc.com>
2024-11-15 11:43:32 -0800

    fix domain vars

--- infra/modules/notifications-email-domain/resources/main.tf
+++ infra/modules/notifications-email-domain/resources/main.tf
@@ -3,9 +3,8 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  mail_from_domain          = "mail.${var.domain_name}"
-  stripped_mail_from_domain = replace(local.mail_from_domain, "/[.]$/", "")
-  dash_domain               = replace(var.domain_name, ".", "-")
+  mail_from_domain = "mail.${var.domain_name}"
+  dash_domain      = replace(var.domain_name, ".", "-")
 }
 
 # Verify email sender identity.
@@ -17,7 +16,7 @@ resource "aws_sesv2_email_identity" "sender_domain" {
 
 # The configuration set applied to messages that is sent through this email channel.
 resource "aws_sesv2_configuration_set" "email" {
-  configuration_set_name = local.dash_domain
+  configuration_set_name = var.domain_name
 
   delivery_options {
     tls_policy = "REQUIRE"
@@ -34,7 +33,7 @@ resource "aws_sesv2_configuration_set" "email" {
 
 resource "aws_sesv2_email_identity_mail_from_attributes" "sender_domain" {
   email_identity   = aws_sesv2_email_identity.sender_domain.email_identity
-  mail_from_domain = local.stripped_mail_from_domain
+  mail_from_domain = local.mail_from_domain
 
   depends_on = [aws_sesv2_email_identity.sender_domain]
 }

b1ea315f0bdcf98f9cb44a3d624a766fc76fef88
Loren Yu <loren@navapbc.com>
2024-12-13 11:43:39 -0800

    Fix channel config by removing extra nesting

--- infra/project-config/system-notifications.tf
+++ infra/project-config/system-notifications.tf
@@ -8,11 +8,9 @@ locals {
     channels = {
       "workflow-failures" = {
         "type" = "slack" # or "teams"
-        "config" = {
-          # Name of the secret in GitHub
-          "channel_id_secret_name"  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
-          "slack_token_secret_name" = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
-        }
+        # Name of the secret in GitHub
+        "channel_id_secret_name"  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
+        "slack_token_secret_name" = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
       }
     }
   }

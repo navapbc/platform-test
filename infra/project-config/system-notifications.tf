89644270fa8c0236852208d2643d7e4d212ae72b
Loren Yu <loren@navapbc.com>
2024-12-13 11:00:28 -0800

    Add default configuration

--- infra/project-config/system-notifications.tf
+++ infra/project-config/system-notifications.tf
@@ -4,19 +4,24 @@ locals {
     }
   }
 
-  channels = {
-    "slack" = {
-      "name" = "slack"
-      "type" = "slack"
-      "config" = {
-        "url" = "https://hooks.slack"
+  system_notifications_config = {
+    channels = {
+      "workflow-failures" = {
+        "type" = "slack" # or "teams"
+        "config" = {
+          # Name of the secret in GitHub
+          "channel_id_secret_name"  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
+          "slack_token_secret_name" = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
+        }
       }
     }
-    "email" = {
-      "name" = "email"
-      "type" = "email"
-      "config" = {
-        "email" = "platform-admins@navapbc.com"
-      }
-    }
+  }
+  # - name: Post to a Slack channel
+  # uses: slackapi/slack-github-action@v2.0.0
+  # with:
+  #   method: chat.postMessage
+  #   token: ${{ secrets.SLACK_BOT_TOKEN }}
+  #   payload: |
+  #     channel: ${{ secrets.SLACK_CHANNEL_ID }}
+  #     text: "howdy <@channel>!"
 }

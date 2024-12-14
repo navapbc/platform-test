5f6f7f6a46d84955f8354bc69034d302c279f3aa
Loren Yu <loren@navapbc.com>
2024-12-13 22:13:40 -0800

    Convert orphaned-prs to run on all apps

---
+++ bin/infra-utils.bash
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------------------------
+# This file contains utility functions for the infrastructure.
+# -----------------------------------------------------------------------------
+
+# Retrieve the names of the applications in the repo by listing the directories in the "infra" directory
+# and filtering out the directories that are not applications.
+# Returns: A list of application names.
+function get_app_names() {
+  find "infra" -maxdepth 1 -type d -not -name "infra" -not -name "accounts" -not -name "modules" -not -name "networks" -not -name "project-config" -not -name "test" -exec basename {} \;
+}

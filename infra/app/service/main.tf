4680bdc6859c1d494039199b9f4f39e649d853ae
Loren Yu <loren@navapbc.com>
2025-01-16 14:28:30 -0800

    Move monitoring stuff to separate file

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -40,13 +40,12 @@ locals {
   # Examples: pull request preview environments are temporary.
   is_temporary = terraform.workspace != "default"
 
-  build_repository_config                        = module.app_config.build_repository_config
-  environment_config                             = module.app_config.environment_configs[var.environment_name]
-  service_config                                 = local.environment_config.service_config
-  database_config                                = local.environment_config.database_config
-  incident_management_service_integration_config = local.environment_config.incident_management_service_integration
-  identity_provider_config                       = local.environment_config.identity_provider_config
-  notifications_config                           = local.environment_config.notifications_config
+  build_repository_config  = module.app_config.build_repository_config
+  environment_config       = module.app_config.environment_configs[var.environment_name]
+  service_config           = local.environment_config.service_config
+  database_config          = local.environment_config.database_config
+  identity_provider_config = local.environment_config.identity_provider_config
+  notifications_config     = local.environment_config.notifications_config
 
   network_config = module.project_config.network_configs[local.environment_config.network_name]
 
@@ -98,13 +97,6 @@ data "aws_iam_policy" "app_db_access_policy" {
 data "aws_iam_policy" "migrator_db_access_policy" {
   count = module.app_config.has_database ? 1 : 0
   name  = local.database_config.migrator_access_policy_name
-}
-
-# Retrieve url for external incident management tool (e.g. Pagerduty, Splunk-On-Call)
-
-data "aws_ssm_parameter" "incident_management_service_integration_url" {
-  count = module.app_config.has_incident_management_service ? 1 : 0
-  name  = local.incident_management_service_integration_config.integration_url_param_name
 }
 
 data "aws_security_groups" "aws_services" {
@@ -202,15 +194,4 @@ module "service" {
   )
 
   is_temporary = local.is_temporary
-}
-
-module "monitoring" {
-  source = "../../modules/monitoring"
-  #Email subscription list:
-  #email_alerts_subscription_list = ["email1@email.com", "email2@email.com"]
-
-  # Module takes service and ALB names to link all alerts with corresponding targets
-  service_name                                = local.service_name
-  load_balancer_arn_suffix                    = module.service.load_balancer_arn_suffix
-  incident_management_service_integration_url = module.app_config.has_incident_management_service && !local.is_temporary ? data.aws_ssm_parameter.incident_management_service_integration_url[0].value : null
 }

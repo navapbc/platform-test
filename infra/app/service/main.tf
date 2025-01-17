25eab628e184c9016b6c722afca4a6802f99cd70
Loren Yu <loren@navapbc.com>
2025-01-17 12:52:50 -0800

    Consolidate domain configs

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -69,7 +69,7 @@ module "service" {
 
   domain_name     = local.domain_name
   hosted_zone_id  = local.hosted_zone_id
-  certificate_arn = local.service_config.enable_https ? data.aws_acm_certificate.certificate[0].arn : null
+  certificate_arn = local.certificate_arn
 
   cpu                      = local.service_config.cpu
   memory                   = local.service_config.memory

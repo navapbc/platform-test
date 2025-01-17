923bd825dfafc1612eefdbd59fb30cd666f8907a
Loren Yu <loren@navapbc.com>
2025-01-17 12:58:06 -0800

    Clean up more

--- infra/app/service/domain.tf
+++ infra/app/service/domain.tf
@@ -1,16 +1,16 @@
 locals {
-  domain_config   = local.environment_config.domain_config
-  domain_name     = local.domain_config.domain_name
-  hosted_zone_id  = local.domain_name != null ? data.aws_route53_zone.zone[0].zone_id : null
+  domain_config = local.environment_config.domain_config
+
+  hosted_zone_id  = local.domain_config.domain_name != null ? data.aws_route53_zone.zone[0].zone_id : null
   certificate_arn = local.domain_config.enable_https ? data.aws_acm_certificate.certificate[0].arn : null
 }
 
 data "aws_acm_certificate" "certificate" {
   count  = local.domain_config.enable_https ? 1 : 0
-  domain = local.domain_name
+  domain = local.domain_config.domain_name
 }
 
 data "aws_route53_zone" "zone" {
-  count = local.domain_name != null ? 1 : 0
+  count = local.domain_config.domain_name != null ? 1 : 0
   name  = local.domain_config.hosted_zone
 }

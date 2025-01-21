e8432aef48d788a5cc22aa6c0c3fff0e04e2cd82
Kai Siren <kaisiren@navapbc.com>
2025-01-21 15:32:29 -0800

    fix resource names

--- infra/modules/service/events_role.tf
+++ infra/modules/service/events_role.tf
@@ -10,7 +10,7 @@ resource "aws_iam_role" "events" {
   assume_role_policy = data.aws_iam_policy_document.events_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachments_exclusive" "example" {
+resource "aws_iam_role_policy_attachments_exclusive" "events" {
   role_name   = aws_iam_role.events.name
   policy_arns = [aws_iam_policy.run_task.arn]
 }
--- infra/modules/service/scheduler_role.tf
+++ infra/modules/service/scheduler_role.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "scheduler" {
   assume_role_policy = data.aws_iam_policy_document.scheduler_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachments_exclusive" "example" {
+resource "aws_iam_role_policy_attachments_exclusive" "scheduler" {
   role_name   = aws_iam_role.scheduler.name
   policy_arns = [aws_iam_policy.scheduler.arn]
 }

52d6971a36c4c17e3f2759ba257ec9e6d6f4b190
Leslie Anderson <leslieanderson@navapbc.com>
2025-02-04 20:02:49 -0500

    rm whitespace

--- app-rails/spec/rails_helper.rb
+++ app-rails/spec/rails_helper.rb
@@ -7,7 +7,6 @@ end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?

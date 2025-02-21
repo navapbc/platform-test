1c5155fd0ac4124440d211fa96795e05795d00a2
Loren Yu <loren@navapbc.com>
2025-02-21 15:28:57 -0800

    Convert secret module to create multiple secrets

---
+++ infra/modules/secrets/main.tf
@@ -0,0 +1,36 @@
+locals {
+  generated_secrets = {
+    for name, config in var.secrets :
+    name => config if config.manage_method == "generated"
+  }
+  manual_secrets = {
+    for name, config in var.secrets :
+    name => config if config.manage_method == "manual"
+  }
+}
+
+resource "random_password" "secrets" {
+  for_each = local.generated_secrets
+
+  length           = 64
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "secrets" {
+  for_each = local.generated_secrets
+
+  name  = each.value.secret_store_name
+  type  = "SecureString"
+  value = random_password.secrets[each.key].result
+
+  tags = {
+    Service = var.service_name
+  }
+}
+
+data "aws_ssm_parameter" "secrets" {
+  for_each = local.manual_secrets
+
+  name = each.value.secret_store_name
+}

8cce5160c1bda94e9d5d40818c49d9b243f8d8fc
Loren Yu <loren@navapbc.com>
2025-02-28 23:51:41 -0800

    Add e2e test for notifications (#185)

---
+++ e2e/app/tests/notifications.spec.ts
@@ -0,0 +1,17 @@
+import { EmailAddress, EmailService } from "../../lib/services/email/EmailService";
+import { expect, test } from "@playwright/test";
+import { MessageCheckerService } from "../../lib/services/email/MessageCheckerService";
+
+test.describe('Email notifications', () => {
+  test('send test email notification', async ({ page, context }) => {
+    const emailService: EmailService = new MessageCheckerService(context);
+    const emailAddress: EmailAddress = emailService.generateEmailAddress();
+
+    await page.goto('/email-notifications');
+    await page.getByRole('textbox').fill(emailAddress);
+    await page.getByRole('button').click();
+
+    const email = await emailService.waitForEmailWithSubject(emailAddress, 'Test notification');
+    expect(email.text).toContain('This is a system generated test notification');
+  });
+});

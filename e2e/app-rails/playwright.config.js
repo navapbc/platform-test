820855d35d11d7803ec1a2d352657de50e7a1174
Loren Yu <loren@navapbc.com>
2025-03-07 11:57:20 -0800

    Use single quotes

--- e2e/app-rails/playwright.config.js
+++ e2e/app-rails/playwright.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig(
   deepMerge(baseConfig, {
     use: {
-      baseURL: baseConfig.use.baseURL || "localhost:3100",
+      baseURL: baseConfig.use.baseURL || 'localhost:3100',
       ignoreHTTPSErrors: true, // Ignore SSL certificate errors
       // emailServiceType: "Mailinator", // Options: ["MessageChecker", "Mailinator"]. Default: "MessageChecker"
     },

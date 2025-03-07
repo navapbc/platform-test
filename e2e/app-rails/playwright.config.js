dedb5d6dfbeef5ce03fa16e9b50804eebc3d7b19
Loren Yu <loren@navapbc.com>
2025-03-07 11:51:52 -0800

    app-rails: Update `template-infra:app` to version 0.15.4

--- e2e/app-rails/playwright.config.js
+++ e2e/app-rails/playwright.config.js
@@ -2,13 +2,12 @@ import baseConfig from '../playwright.config';
 import { deepMerge } from '../lib/util';
 import { defineConfig } from '@playwright/test';
 
-export default defineConfig(deepMerge(
-  baseConfig,
-  {
+export default defineConfig(
+  deepMerge(baseConfig, {
     use: {
       baseURL: baseConfig.use.baseURL || "localhost:3100",
       ignoreHTTPSErrors: true, // Ignore SSL certificate errors
       // emailServiceType: "Mailinator", // Options: ["MessageChecker", "Mailinator"]. Default: "MessageChecker"
     },
-  }
-));
+  })
+);

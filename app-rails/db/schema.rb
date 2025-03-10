fdb88243c058b233c9e3f2a174bde6a0f7654e59
Tanner Doshier <git@doshitan.com>
2025-03-10 15:23:35 -0400

    Rails 7.2

--- app-rails/db/schema.rb
+++ app-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_213056) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_10_140243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

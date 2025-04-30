f939239699183a27ff6b54d5e455f70a3b0524f3
Loren Yu <loren@navapbc.com>
2025-04-30 09:04:11 -0700

    Add component_preview layout

--- app-rails/app/previews/breadcrumbs_preview.rb
+++ app-rails/app/previews/breadcrumbs_preview.rb
@@ -1,6 +1,8 @@
 class BreadcrumbsPreview < Lookbook::Preview
+  layout "component_preview"
+
   def default
-    result = render template: "application/_breadcrumbs", locals: { crumbs: [
+    render template: "application/_breadcrumbs", locals: { crumbs: [
       { name: "Passport applications", url: "https://google.com" },
       ], current_name: "New passport application" } 
   end

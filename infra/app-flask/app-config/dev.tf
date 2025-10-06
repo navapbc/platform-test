e325304c45a787404b4b8be7b2a1a578f9aff169
Sean Thomas <sean.thomas@navapbc.com>
2025-10-06 16:54:20 -0400

    app-flask: Install `template-infra:app` at version 0.15.6.post11.dev0+9e17317

---
+++ infra/app-flask/app-config/dev.tf
@@ -0,0 +1,30 @@
+module "dev_config" {
+  source                          = "./env-config"
+  project_name                    = local.project_name
+  app_name                        = local.app_name
+  default_region                  = module.project_config.default_region
+  environment                     = "dev"
+  network_name                    = "dev"
+  domain_name                     = null
+  enable_https                    = false
+  has_database                    = local.has_database
+  has_incident_management_service = local.has_incident_management_service
+  enable_notifications            = local.enable_notifications
+
+  # Enable and configure identity provider.
+  enable_identity_provider = local.enable_identity_provider
+
+  # Support local development against the dev instance.
+  extra_identity_provider_callback_urls = ["http://localhost"]
+  extra_identity_provider_logout_urls   = ["http://localhost"]
+
+  # Enables ECS Exec access for debugging or jump access.
+  # See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-exec.html
+  # Defaults to `false`. Uncomment the next line to enable.
+  # enable_command_execution = true
+
+  # Uncomment to override default feature flag values
+  # feature_flag_overrides = {
+  #   BAR = true
+  # }
+}
---
+++ infra/app-flask/app-config/outputs.tf
@@ -0,0 +1,43 @@
+output "app_name" {
+  value = local.app_name
+}
+
+output "build_repository_config" {
+  value = local.build_repository_config
+}
+
+output "environment_configs" {
+  value = local.environment_configs
+}
+
+output "environments" {
+  value = local.environments
+}
+
+output "has_database" {
+  value = local.has_database
+}
+
+output "has_external_non_aws_service" {
+  value = local.has_external_non_aws_service
+}
+
+output "has_incident_management_service" {
+  value = local.has_incident_management_service
+}
+
+output "enable_identity_provider" {
+  value = local.enable_identity_provider
+}
+
+output "enable_notifications" {
+  value = local.enable_notifications
+}
+
+output "enable_waf" {
+  value = local.enable_waf
+}
+
+output "shared_network_name" {
+  value = local.shared_network_name
+}

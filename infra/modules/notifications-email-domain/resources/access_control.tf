c56a6c528e1feeffddb195e2564ced052086aba4
Sean Thomas <sean.thomas@navapbc.com>
2025-10-20 09:21:30 -0400

    [942] Remove unused outputs from notifications-email-domain modules

--- infra/modules/notifications-email-domain/resources/access_control.tf
+++
@@ -1,3 +0,0 @@
-# Resource-based policy removed - applications now access SES directly via IAM policies
-# Previously allowed Pinpoint to send email on behalf of this email identity
-# No resource-based policy is needed since applications use their own IAM credentials
--- infra/modules/notifications-email-domain/resources/outputs.tf
+++ infra/modules/notifications-email-domain/resources/outputs.tf
@@ -1,11 +1,3 @@
 output "domain_identity_arn" {
   value = aws_sesv2_email_identity.sender_domain.arn
 }
-
-output "configuration_set_name" {
-  value = aws_sesv2_configuration_set.email.configuration_set_name
-}
-
-output "email_identity" {
-  value = aws_sesv2_email_identity.sender_domain.email_identity
-}

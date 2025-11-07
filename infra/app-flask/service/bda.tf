a2a48b58ab84d699eb1967c719ed1a20a0ce9b91
Damian Jimenez <damianjimenez@navapbc.com>
2025-11-07 15:10:09 -0600

    delete resources

--- infra/app-flask/service/bda.tf
+++ infra/app-flask/service/bda.tf
@@ -1,48 +1,48 @@
-module "bda_input_bucket" {
-  source       = "../../modules/storage"
-  name         = "bda-test-input-app-flask"
-  is_temporary = local.is_temporary
-}
+# module "bda_input_bucket" {
+#   source       = "../../modules/storage"
+#   name         = "bda-test-input-app-flask"
+#   is_temporary = local.is_temporary
+# }
 
-module "bda_output_bucket" {
-  source       = "../../modules/storage"
-  name         = "bda-test-output-app-flask"
-  is_temporary = local.is_temporary
-}
+# module "bda_output_bucket" {
+#   source       = "../../modules/storage"
+#   name         = "bda-test-output-app-flask"
+#   is_temporary = local.is_temporary
+# }
 
-module "bda_test" {
-  source = "../../modules/bedrock-data-automation"
+# module "bda_test" {
+#   source = "../../modules/bedrock-data-automation"
 
-  bda_standard_output_configuration = {
-    image = {
-      extraction = {
-        bounding_box = {
-          state = "ENABLED"
-        }
-        category = {
-          state = "ENABLED"
-          types = ["TEXT_DETECTION", "LOGOS"]
-        }
-      }
-      generative_field = {
-        state = "ENABLED"
-        types = ["IMAGE_SUMMARY"]
-      }
-    }
-  }
+#   bda_standard_output_configuration = {
+#     image = {
+#       extraction = {
+#         bounding_box = {
+#           state = "ENABLED"
+#         }
+#         category = {
+#           state = "ENABLED"
+#           types = ["TEXT_DETECTION", "LOGOS"]
+#         }
+#       }
+#       generative_field = {
+#         state = "ENABLED"
+#         types = ["IMAGE_SUMMARY"]
+#       }
+#     }
+#   }
 
-  blueprints_map = {
-    "drivers_license" = {
-      schema                 = file("blueprints/template_blueprint.json")
-      type                   = "DOCUMENT"
-      kms_encryption_context = null
-      kms_key_id             = null
-      tags                   = [{ key = "example_tag_key", value = "example_tag_value" }]
-    }
-  }
-  name_prefix = "bda_module_test"
-  bucket_policy_arns = {
-    input_bucket  = module.bda_input_bucket.access_policy_arn,
-    output_bucket = module.bda_output_bucket.access_policy_arn
-  }
-}
+#   blueprints_map = {
+#     "drivers_license" = {
+#       schema                 = file("blueprints/template_blueprint.json")
+#       type                   = "DOCUMENT"
+#       kms_encryption_context = null
+#       kms_key_id             = null
+#       tags                   = [{ key = "example_tag_key", value = "example_tag_value" }]
+#     }
+#   }
+#   name_prefix = "bda_module_test"
+#   bucket_policy_arns = {
+#     input_bucket  = module.bda_input_bucket.access_policy_arn,
+#     output_bucket = module.bda_output_bucket.access_policy_arn
+#   }
+# }

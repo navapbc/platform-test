24aab26e15fbc58a9877e21e63616f367fc8ed67
Laurence Goolsby <laurencegoolsby@navapbc.com>
2026-01-29 13:58:35 -0500

    Add KMS encryption options to storage module
    - Add use_aws_managed_encryption variable to toggle between customer-managed KMS keys and AWS-managed encryption (AES256)
    - Add kms_s3_via_service_principals variable to grant AWS services (e.g., bedrock.amazonaws.com) access to KMS key via S3
    - Update IAM access policy to conditionally include KMS permissions based on encryption type
    - Default behavior remains customer-managed KMS encryption

--- infra/modules/storage/outputs.tf
+++ infra/modules/storage/outputs.tf
@@ -1,3 +1,7 @@
 output "access_policy_arn" {
   value = aws_iam_policy.storage_access.arn
 }
+
+output "kms_key_arn" {
+  value = var.use_aws_managed_encryption ? null : aws_kms_key.storage[0].arn
+}

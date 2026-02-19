17c3b069d3b6113e9954845157df9ec68f734c90
jrpbc <johanrobles@navapbc.com>
2026-02-18 20:31:17 -0500

    Fix CloudFormation destroy dependency ordering

--- infra/modules/notifications-sms/resources/outputs.tf
+++ infra/modules/notifications-sms/resources/outputs.tf
@@ -1,6 +1,6 @@
 output "configuration_set_name" {
   description = "The name of the AWS End User Messaging SMS configuration set."
-  value       = aws_cloudformation_stack.sms_config_set.outputs["ConfigSetName"]
+  value       = data.aws_cloudformation_stack.sms_config_set_outputs.outputs["ConfigSetName"]
 }
 
 output "access_policy_arn" {
@@ -8,22 +8,27 @@ output "access_policy_arn" {
   value       = aws_iam_policy.sms_access.arn
 }
 
+output "cloudformation_service_role_arn" {
+  description = "The ARN of the CloudFormation service role used for SMS resource management."
+  value       = aws_iam_role.cloudformation_service_role.arn
+}
+
 output "sms_phone_pool_arn" {
   description = "The ARN of the AWS End User Messaging SMS phone pool."
-  value       = aws_cloudformation_stack.sms_config_set.outputs["PhonePoolArn"]
+  value       = data.aws_cloudformation_stack.sms_config_set_outputs.outputs["PhonePoolArn"]
 }
 
 output "sms_phone_pool_id" {
   description = "The ID of the AWS End User Messaging SMS phone pool."
-  value       = aws_cloudformation_stack.sms_config_set.outputs["PhonePoolId"]
+  value       = data.aws_cloudformation_stack.sms_config_set_outputs.outputs["PhonePoolId"]
 }
 
 output "sms_phone_number_id" {
   description = "The ID of the SMS phone number created by CloudFormation."
-  value       = aws_cloudformation_stack.sms_config_set.outputs["PhoneNumberId"]
+  value       = data.aws_cloudformation_stack.sms_config_set_outputs.outputs["PhoneNumberId"]
 }
 
 output "sms_phone_number_arn" {
   description = "The ARN of the SMS phone number created by CloudFormation."
-  value       = aws_cloudformation_stack.sms_config_set.outputs["PhoneNumberArn"]
+  value       = data.aws_cloudformation_stack.sms_config_set_outputs.outputs["PhoneNumberArn"]
 }

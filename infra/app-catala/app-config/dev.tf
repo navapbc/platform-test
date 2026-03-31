c26dfc753828562846764bc6daffe13f44d8c2e9
Roger Lam <rogerlam@navapbc.com>
2026-03-31 13:52:36 -0700

    Add custom domain for app-catala in dev

--- infra/app-catala/app-config/dev.tf
+++ infra/app-catala/app-config/dev.tf
@@ -5,7 +5,7 @@ module "dev_config" {
   default_region                  = module.project_config.default_region
   environment                     = "dev"
   network_name                    = "dev"
-  domain_name                     = null
+  domain_name                     = "${local.app_name}.${module.project_config.network_configs["dev"].domain_config.hosted_zone}"
   enable_https                    = false
   has_database                    = local.has_database
   has_incident_management_service = local.has_incident_management_service

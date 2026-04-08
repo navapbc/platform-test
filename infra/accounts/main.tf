47afa910751a8baedccbde820dae4523a75b12d6
Sean Thomas <sean.thomas@navapbc.com>
2026-04-08 19:17:12 -0400

    Revert "Fix S3 lockfile not honored via partial backend config (#269)"
    This reverts commit 4a3d20f1bf02b35fc5828fb536a27ccf894ca40d.

--- infra/accounts/main.tf
+++ infra/accounts/main.tf
@@ -26,8 +26,7 @@ terraform {
   }
 
   backend "s3" {
-    use_lockfile = true
-    encrypt      = "true"
+    encrypt = "true"
   }
 }
 
--- infra/app-flask/service/main.tf
+++ infra/app-flask/service/main.tf
@@ -35,8 +35,7 @@ terraform {
   }
 
   backend "s3" {
-    use_lockfile = true
-    encrypt      = "true"
+    encrypt = "true"
   }
 }
 
